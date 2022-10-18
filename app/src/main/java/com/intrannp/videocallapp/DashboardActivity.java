ea2bfcd92c383590ea4ca641bdfbbee6bb3eefab
Deepak Yadav <74250878+y-deepak@users.noreply.github.com>
2022-10-18 19:34:52 +0530

    Initial commit

---
+++ app/src/main/java/com/intrannp/videocallapp/DashboardActivity.java
@@ -0,0 +1,71 @@
+package com.intrannp.videocallapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    EditText secretCodeBox;
+    Button joinBtn, createBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        secretCodeBox = findViewById(R.id.codeBox);
+        joinBtn = findViewById(R.id.joinBtn);
+        createBtn = findViewById(R.id.createBtn);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GENERATE();
+            }
+
+            private void GENERATE() {
+                RandomString randomString= new RandomString();
+                String result=randomString.generateAlphaNumeric(6);
+                secretCodeBox.setText(result);
+            }
+        });
+
+        URL serverURL;
+
+        try {
+            serverURL = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions =
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setServerURL(serverURL)
+                            .setWelcomePageEnabled(false)
+                            .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(secretCodeBox.getText().toString())
+                        .setWelcomePageEnabled(false)
+                        .build();
+
+                JitsiMeetActivity.launch(DashboardActivity.this, options);
+            }
+        });
+    }
+}
